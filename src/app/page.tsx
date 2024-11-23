6f0b6acc8fc7314f63caaf619496cf89ee053ee9
w3tsadev <115426204+w3tsadev@users.noreply.github.com>
2024-11-22 21:15:24 -0500

    Added border className on the Page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
               <div className="flex flex-col w-full md:flex-row gap-2 text-nowrap">
                 <Link
                   href="/search/womens-collection"
-                  className="inline-flex h-9 items-center justify-center rounded-md bg-primary px-4 py-2 text-sm font-medium text-primary-foreground shadow transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
+                  className="inline-flex h-9 items-center justify-center rounded-md border bg-primary px-4 py-2 text-sm font-medium text-primary-foreground shadow transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
                   prefetch={false}
                 >
                   Shop Women
