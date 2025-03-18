079b2cdbe6792ebe4fd8a22b4db1dbea0365209d
Frans701 <franswahyu7@gmail.com>
2025-03-18 15:31:18 +0800

    our-mission-homepage

--- src/components/pages/homepage/OurMission.tsx
+++ src/components/pages/homepage/OurMission.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Maroon from "@/components/icons/Maroon";
+import Button from "@/components/ui/button";
 import GradientImage from "@/components/ui/GradientImage";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
@@ -117,7 +118,7 @@ export default function OurMission() {
     tl1.to(
       ".container-chocolate-at-cocolily",
       {
-        top: "16.5%",
+        top: "27%",
         ease: "power1.inOut",
       },
       "<"
@@ -145,22 +146,111 @@ export default function OurMission() {
       },
       "<"
     );
+
+    // section 4
+    tl1.to(".container-chocolate-box-at-cocolily", {
+      bottom: "53%",
+      ease: "power1.inOut",
+    });
+    tl1.to(
+      ".container-chocolate-at-cocolily",
+      {
+        top: "40%",
+        ease: "power1.inOut",
+      },
+      "<"
+    );
+
+    // section 5
+    tl1.to(".container-chocolate-box-at-cocolily", {
+      bottom: "60%",
+      ease: "power1.inOut",
+    });
+    tl1.to(
+      ".container-chocolate-at-cocolily",
+      {
+        right: "31.2%",
+        rotate: 15,
+        scale: 0.5,
+        top: "59.2%",
+        ease: "power1.inOut",
+      },
+      "<"
+    );
+    tl1.to(
+      ".shadow-chocolate",
+      {
+        autoAlpha: 0,
+        ease: "power1.inOut",
+      },
+      "<"
+    );
+    tl1.to(
+      ".container-chocolate-box-top-at-cocolily",
+      {
+        top: "34%",
+        ease: "power1.inOut",
+      },
+      "<"
+    );
+
+    // section 6
+    tl1.to(".container-chocolate-bottom", {
+      top: "-10.6601%",
+      ease: "power1.inOut",
+    });
+    tl1.to(
+      ".container-chocolate-box-top-at-cocolily",
+      {
+        top: "63.5436%",
+        ease: "power1.inOut",
+      },
+      "<"
+    );
   }, []);
 
   return (
     <>
       <div className="w-full h-screen"></div>
-      <div className="w-full h-screen font-span bg-ruby-red container-at-cocolily">
-        <div className="absolute w-165d h-111d -translate-y-1/2 top-1/2 right-[29%] container-chocolate-at-cocolily">
-          <div className="w-full h-full chocolate-at-cocolily">
-            <GradientImage
-              src="/images/our-mission/Chocolate.png"
-              fitVariant="contain"
-            />
+      <div className="w-full h-screen font-span bg-ruby-red container-at-cocolily overflow-hidden">
+        <div className="w-full h-1080d absolute top-1/2 -translate-y-1/2 z-10 pointer-events-none">
+          <div className="absolute z-20 w-544d h-370d -translate-y-full top-[0%] right-[29%] container-chocolate-box-top-at-cocolily">
+            <div className="w-full h-full chocolate-box-top-at-cocolily">
+              <GradientImage
+                src="/images/our-mission/Chocolate-box-top.png"
+                fitVariant="contain"
+              />
+            </div>
+          </div>
+          <div className="absolute w-full z-10 h-full container-chocolate-bottom">
+            <div className="absolute z-20 w-165d h-111d -translate-y-1/2 top-[51%] right-[29%] container-chocolate-at-cocolily">
+              <div className="w-full h-full chocolate-at-cocolily relative">
+                <div className="w-full h-full absolute top-0 z-10">
+                  <GradientImage
+                    src="/images/our-mission/Chocolate.png"
+                    fitVariant="contain"
+                  />
+                </div>
+                <div className="w-full h-full absolute bottom-[-32%] left-[9%] scale-[0.8] shadow-chocolate">
+                  <GradientImage
+                    src="/images/our-mission/Chocolate-shadow.png"
+                    fitVariant="contain"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="absolute z-10 w-544d h-370d translate-y-full bottom-[0%] right-[29%] container-chocolate-box-at-cocolily">
+              <div className="w-full h-full chocolate-box-at-cocolily">
+                <GradientImage
+                  src="/images/our-mission/Chocolate-box.png"
+                  fitVariant="contain"
+                />
+              </div>
+            </div>
           </div>
         </div>
-        <div className="w-full h-full flex flex-col justify-center items-center text-center gap-[15vh]">
-          <div className="relative w-full">
+        <div className="w-full h-full text-center">
+          <div className="absolute w-full top-[15vh] left-1/2 -translate-x-1/2">
             <p className="font-abc uppercase text-seashell font-light text-anim-at-cocolily">
               at cocolily
             </p>
@@ -168,7 +258,7 @@ export default function OurMission() {
               our mission
             </p>
           </div>
-          <div className="relative w-full">
+          <div className="absolute w-full top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2">
             <div className="text-seashell font-light w-full">
               <h2 className="text-anim-at-cocolily">
                 We create <span className="italic">more</span>
@@ -191,9 +281,9 @@ export default function OurMission() {
               <div className="relative">
                 <h2 className="text-anim-at-cocolily-01">
                   <span className="text-anim-at-cocolily-is-to-celebrate inline-block">
-                    Is to <span className="italic">celebrate </span>
+                    Is to <span className="italic">celebrate</span>
                   </span>
-                  <span className="text-anim-at-cocolily-sweetness inline-block">
+                  <span className="text-anim-at-cocolily-sweetness inline-block font-semibold">
                     sweetness
                   </span>
                 </h2>
@@ -238,7 +328,19 @@ export default function OurMission() {
               </div>
             </div>
           </div>
-          <div className="maroon-at-cocolily">
+          <div className="absolute bottom-[19vh] left-1/2 -translate-x-1/2">
+            <div className="text-anim-at-cocolily-01 py-12d">
+              <Button
+                buttonType="link"
+                href="/"
+                isHoverTranslate
+                className="w-max uppercase font-sans"
+              >
+                more about us
+              </Button>
+            </div>
+          </div>
+          <div className="absolute bottom-[8vh] left-1/2 -translate-x-1/2 maroon-at-cocolily">
             <Maroon />
           </div>
         </div>
