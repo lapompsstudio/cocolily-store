92b025b69ca5d5530f86829c6361769d6a9caa2d
Arta Kusuma <arta.atmaja@gmail.com>
2025-03-28 15:39:50 +0800

    feat: integrate global data into footer and template components - add: axios and @tanstack/react-query for data fetching - update: footer to display dynamic global data (address, email, phone, social links) - refactor: template to fetch global data on mount

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -4,11 +4,17 @@ import React, { useEffect } from "react";
 import { ReactLenis } from "lenis/react";
 import { Navbar } from "@/components/layout/navbar";
 import Preloader from "@/components/preloader";
+import { globalStore } from "@/store/globalStore";
 
 const Template = ({ children }: { children: React.ReactNode }) => {
+  const { fetchGlobalData, isGlobalFetched } = globalStore();
+
   useEffect(() => {
     window.scrollTo(0, 0);
-  }, []);
+    if (!isGlobalFetched) {
+      fetchGlobalData();
+    }
+  }, [fetchGlobalData, isGlobalFetched]);
 
   return (
     <ReactLenis root>
