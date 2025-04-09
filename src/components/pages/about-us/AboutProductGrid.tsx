8949a79c5db5d685561e619d7d7a530131ac6ab6
Arta Kusuma <arta.atmaja@gmail.com>
2025-04-09 14:01:16 +0800

    feat: create About Us page with individual components placeholder

---
+++ src/components/pages/about-us/AboutProductGrid.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AboutProductGrid = () => {
+  return (
+    <div className="h-screen bg-baby-pink grid place-content-center font-bold text-ruby-red">
+      AboutProductGrid
+    </div>
+  );
+};
+
+export default AboutProductGrid;
---
+++ src/components/pages/about-us/AboutPurpose.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AboutPurpose = () => {
+  return (
+    <div className="h-screen bg-baby-pink grid place-content-center font-bold text-ruby-red">
+      AboutPurpose
+    </div>
+  );
+};
+
+export default AboutPurpose;
