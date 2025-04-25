3952565726b46ff571698994e96770bb583fa898
gungandre <gungd86@gmail.com>
2025-04-25 09:21:56 +0800

    eventsList progress

--- src/components/pages/events/Events.tsx
+++ src/components/pages/events/Events.tsx
@@ -3,10 +3,12 @@ import React from "react";
 import EventsHero from "./EventsHero";
 import EventsList from "./EventsList";
 import Footer from "@/components/layout/footer";
+import CursorFollow from "@/components/ui/CursorFollow";
 
 const Events = () => {
   return (
     <>
+      <CursorFollow />
       <EventsHero />
       <EventsList />
       <Footer />
