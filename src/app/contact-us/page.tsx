189f479aa586eb5ff8e385a9539b6f16cf3bb3b3
Arta Kusuma <arta.atmaja@gmail.com>
2025-04-28 14:25:46 +0800

    setup: contact page

---
+++ src/app/contact-us/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import ContactUs from "@/components/pages/contact-us/ContactUs";
+
+const ContactUsPage = () => {
+  return <ContactUs />;
+};
+
+export default ContactUsPage;
