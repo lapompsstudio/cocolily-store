c58d30de2bd102007b87621aa7f137d3bde85481
Arta Kusuma <arta.atmaja@gmail.com>
2025-05-02 10:21:50 +0800

    setup: creation detail page

---
+++ src/components/pages/creations/details/CreationProductRelated.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CreationProductRelated = () => {
+  return (
+    <div className="h-screen text-ruby-red font-bold grid place-content-center">
+      CreationProductRelated
+    </div>
+  );
+};
+
+export default CreationProductRelated;
