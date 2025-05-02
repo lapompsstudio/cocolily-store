c71e9eddfc4d17d607a371af543b84e907265df7
Arta Kusuma <arta.atmaja@gmail.com>
2025-05-02 10:21:50 +0800

    setup: creation detail page

---
+++ src/app/creations/[handle]/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import CreationProductDetail from "@/components/pages/creations/details/CreationProductDetail";
+import CreationProductRelated from "@/components/pages/creations/details/CreationProductRelated";
+
+const CreationsDetailPage = () => {
+  return (
+    <>
+      <CreationProductDetail />
+      <CreationProductRelated />
+    </>
+  );
+};
+
+export default CreationsDetailPage;
---
+++ src/components/pages/creations/details/CreationProductDetail.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CreationProductDetail = () => {
+  return (
+    <div className="h-screen text-ruby-red font-bold grid place-content-center">
+      CreationProductDetail
+    </div>
+  );
+};
+
+export default CreationProductDetail;
