f7c075a3624dc91cce98fdf41f8371c7b902a3cd
Arta Kusuma <arta.atmaja@gmail.com>
2025-05-05 15:14:38 +0800

    setup: event detail

---
+++ src/components/pages/events/details/EventDetailSummary.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const EventDetailSummary = () => {
+  return (
+    <div className="h-screen grid place-content-center text-ruby-red font-bold">
+      EventDetailSummary
+    </div>
+  );
+};
+
+export default EventDetailSummary;
