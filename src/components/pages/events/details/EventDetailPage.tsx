d77eeddf4b12d6d355b98abea5f02acd4a552c54
gungandre <gungd86@gmail.com>
2025-05-08 14:46:01 +0800

    update: slicing experience section

--- src/components/pages/events/details/EventDetailPage.tsx
+++ src/components/pages/events/details/EventDetailPage.tsx
@@ -1,9 +1,11 @@
 "use client";
 
 import React, { useEffect } from "react";
+import { useParams } from "next/navigation";
 
 import useColorStore from "@/store/colorStore";
 import { useFooterStore } from "@/store/footerStore";
+import { useQuery } from "@tanstack/react-query";
 
 import EventDetailHero from "./EventDetailHero";
 import EventDetailHighlight from "./EventDetailHighlight";
@@ -12,10 +14,27 @@ import EventDetailGallery from "./EventDetailGallery";
 import EventDetailSummary from "./EventDetailSummary";
 import Footer from "@/components/layout/footer";
 import EventDetailHighlightDesc from "./EventDetailHighlightDesc";
+import { EventDetail } from "@/types/api";
 
 const EventDetailPage = () => {
   const { isRenderFooter, initFooter } = useFooterStore();
   const { setColor } = useColorStore();
+  const params = useParams();
+
+  const { data } = useQuery<EventDetail>({
+    queryKey: ["event-details"],
+    queryFn: async () => {
+      const res = await fetch("/api/event-details", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ documentId: params.handle }),
+      });
+      if (!res.ok) throw new Error("Network response was not ok");
+      return res.json();
+    },
+  });
 
   useEffect(() => {
     setColor(4);
@@ -27,7 +46,7 @@ const EventDetailPage = () => {
       <EventDetailHero />
       <EventDetailHighlight />
       <EventDetailHighlightDesc />
-      <EventDetailExperience />
+      {data && <EventDetailExperience data={data} />}
       <EventDetailGallery />
       <EventDetailSummary />
       {isRenderFooter && <Footer />}
--- src/types/api.ts
+++ src/types/api.ts
@@ -382,3 +382,96 @@ export type EventResponse = {
     };
   };
 };
+
+export type EventDetail = {
+  data: {
+    id: number;
+    documentId: string;
+    eventName: string;
+    location: string;
+    date: string;
+    description: string;
+    EndDate: string;
+    createdAt: string;
+    updatedAt: string;
+    publishedAt: string;
+    Experience: {
+      id: number;
+      title: string;
+      description: string;
+      image: {
+        id: number;
+        image: {
+          id: number;
+          documentId: string;
+          name: string;
+          alternativeText: string | null;
+          caption: string | null;
+          width: number;
+          height: number;
+          formats: {
+            large?: {
+              ext: string;
+              url: string;
+              hash: string;
+              mime: string;
+              name: string;
+              path: string | null;
+              size: number;
+              width: number;
+              height: number;
+              sizeInBytes: number;
+            };
+            medium?: {
+              ext: string;
+              url: string;
+              hash: string;
+              mime: string;
+              name: string;
+              path: string | null;
+              size: number;
+              width: number;
+              height: number;
+              sizeInBytes: number;
+            };
+            small?: {
+              ext: string;
+              url: string;
+              hash: string;
+              mime: string;
+              name: string;
+              path: string | null;
+              size: number;
+              width: number;
+              height: number;
+              sizeInBytes: number;
+            };
+            thumbnail?: {
+              ext: string;
+              url: string;
+              hash: string;
+              mime: string;
+              name: string;
+              path: string | null;
+              size: number;
+              width: number;
+              height: number;
+              sizeInBytes: number;
+            };
+          };
+          hash: string;
+          ext: string;
+          mime: string;
+          size: number;
+          url: string;
+          previewUrl: string | null;
+          provider: string;
+          provider_metadata: any;
+          createdAt: string;
+          updatedAt: string;
+          publishedAt: string;
+        };
+      }[];
+    }[];
+  };
+};
