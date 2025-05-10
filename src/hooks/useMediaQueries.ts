d1cdbe590aebad023275245a424ab834e52e506e
gungandre <gungd86@gmail.com>
2025-05-10 14:25:37 +0800

    responsive: hero section and aboutWhatDone about-us page

--- src/hooks/useMediaQueries.ts
+++ src/hooks/useMediaQueries.ts
@@ -2,18 +2,23 @@ import { useState, useEffect } from "react";
 
 function useMediaQueries() {
   const [isMobile, setIsMobile] = useState<boolean | null>(null);
+  const [isTablet, setIsTablet] = useState<boolean | null>(null);
   const [isDesktop, setIsDesktop] = useState<boolean | null>(null);
 
   useEffect(() => {
     const updateMediaQueries = () => {
       const mobileQuery = window.matchMedia(
-        "(max-width: 1279px) and (orientation: portrait)"
+        "(max-width: 767px) and (orientation: portrait)"
+      ).matches;
+      const tabletQuery = window.matchMedia(
+        "(min-width: 768px) and (orientation: portrait)"
       ).matches;
       const desktopQuery = window.matchMedia(
         "(min-width: 768px) and (orientation: landscape)"
       ).matches;
 
       setIsMobile(mobileQuery);
+      setIsTablet(tabletQuery);
       setIsDesktop(desktopQuery);
     };
 
@@ -26,6 +31,7 @@ function useMediaQueries() {
 
   return {
     isMobile,
+    isTablet,
     isDesktop,
   };
 }
